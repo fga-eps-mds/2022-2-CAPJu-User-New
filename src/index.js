1a2a1c5bddd6599902e8d0a86ca0124096628934
Lude Ribeiro <ludeyuri07@gmail.com>
2023-01-17 23:50:51 -0300

    Adicionando ddependencias base para começar o desenvolvimento

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import swaggerUI from 'swagger-ui-express';
+import express from "express";
+import cors from "cors";
+import routes from "./routes.js";
+import swaggerFile from "./swagger/swaggerFile.js";
+
+const app = express();
+app.use(express.json({ limit: "50mb" }));
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+app.use(
+    '/api/v1/docs',
+    swaggerUI.serve,
+    swaggerUI.setup(swaggerFile),
+);
+
+export default app;
